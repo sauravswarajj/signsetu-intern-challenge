a99d8823abcdb419f61bab2282c1e0b4d568164d
sauravswarajj <sauravswaraj.dev@gmail.com>
2025-09-15 22:28:09 +0530

    fixed signout redirect to homepage now previous was on login page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
 
   const handleSignOut = async () => {
     await supabase.auth.signOut()
-    router.push('/login')
+    router.push('/')
     router.refresh()
   }
 
